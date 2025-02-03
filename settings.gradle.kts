5f1afade2dba750f6c62357ce50ac9f347981b3f
INFO1-EQUIP25\Tarda <A23llusanint@inspedralbes.cat>
2025-02-03 16:06:58 +0100

    no message

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Roger_Santana_UF1_ACT20"
+include(":app")
+ 
